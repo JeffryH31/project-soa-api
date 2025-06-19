950d2baec87fe9857fc7efb81a62281c0872c4a9
MarvelFanuel <c14220113@john.petra.ac.id>
2025-06-19 14:18:59 +0700

    api eventreservation

--- app/Http/Controllers/EventReservationController.php
+++ app/Http/Controllers/EventReservationController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\EventReservation;
 use Illuminate\Http\Request;
 
-class EventReservationController extends Controller
+class EventReservationController extends BaseController
 {
-    //
+    public function __construct(EventReservation $model)
+    {
+        parent::__construct($model);
+    }
+
+
 }
--- routes/api.php
+++ routes/api.php
@@ -6,12 +6,20 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EventPackageController;
 use App\Http\Controllers\DishCategoryController;
 use App\Http\Controllers\EventMenuController;
+use App\Http\Controllers\EventReservationController;
 
 Route::apiResource('event_spaces', EventSpaceController::class);
 Route::apiResource('event_packages', EventPackageController::class);
 Route::apiResource('dish_categories', DishCategoryController::class);
 Route::apiResource('event_menus', EventMenuController::class);
-Route::get('event_packages/search', [EventPackageController::class, 'search']);
+Route::apiResource('event_reservations', EventReservationController::class);
+
+
+// Route::post('event_menus/upload', [EventMenuController::class, 'store']);
+// Route::get('event_spaces/search', [EventSpaceController::class, 'search']);
+// Route::get('event_menus/search', [EventMenuController::class, 'search']);
+// Route::get('dish_categories/search', [DishCategoryController::class, 'search']);
+// Route::get('event_packages/search', [EventPackageController::class, 'search']);
 
 // Event Package Routes
 // Route::prefix('event-packages')->group(function () {
