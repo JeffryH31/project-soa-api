3c971d9e6c31778b5cd97e43471aec744700ed6d
Jeffry Hermawan <jeffryhermawantm@gmail.com>
2025-06-19 20:53:43 +0700

    fix: create and store reserv

--- app/Models/EventReservation.php
+++ app/Models/EventReservation.php
@@ -19,6 +19,8 @@ class EventReservation extends Model
         'total_price',
         'status',
         'event_space_id',
+        'event_menu_id',
+        // 'event_add_ons_id',
     ];
 
     protected $casts = [
@@ -31,12 +33,16 @@ class EventReservation extends Model
         return [
             'customer_name' => 'required|string|max:255',
             // 'event_package_id' => 'required|uuid|exists:event_packages,id',
-            'event_date' => 'required|date',
+            'start_date' => 'required|date',
+            'end_date' => 'required|date|after_or_equal:start_date',
             'notes' => 'nullable|string|max:500',
-            'total_price' => 'required|numeric|min:0',
-            'status' => 'required|in:pending,confirmed,cancelled',
+            'pax' => 'required|numeric|max:500',
+            'total_price' => 'nullable|numeric|min:0',
+            'event_space_id' => 'required|uuid|exists:event_spaces,id',
+            'status' => 'nullable|in:pending,dp1,dp2,paid,cancelled',
         ];
     }
+
     public function validationMessages()
     {
         return [
@@ -48,8 +54,12 @@ class EventReservation extends Model
             // 'event_package_id.uuid' => 'ID paket acara harus berupa UUID.',
             // 'event_package_id.exists' => 'Paket acara yang dipilih tidak ditemukan.',
 
-            'event_date.required' => 'Tanggal acara harus diisi.',
-            'event_date.date' => 'Tanggal acara harus berupa tanggal yang valid.',
+            'start_date.required' => 'Tanggal mulai acara harus diisi.',
+            'start_date.date' => 'Tanggal mulai acara harus berupa tanggal yang valid.',
+
+            'end_date.required' => 'Tanggal berakhir acara harus diisi.',
+            'end_date.date' => 'Tanggal berakhir acara harus berupa tanggal yang valid.',
+            'end_date.after_or_equal' => 'Tanggal berakhir acara harus sama dengan atau setelah tanggal mulai acara.',
 
             'notes.string' => 'Permintaan khusus harus berupa teks.',
             'notes.max' => 'Permintaan khusus maksimal 500 karakter.',
@@ -60,6 +70,10 @@ class EventReservation extends Model
 
             'status.required' => 'Status harus diisi.',
             'status.in' => 'Status harus salah satu dari: pending, confirmed, cancelled.',
+
+            'event_space_id.required' => 'Event Space harus dipilih.',
+            'event_space_id.uuid' => 'ID Event Space harus valid UUID.',
+            'event_space_id.exists' => 'Event Space yang dipilih tidak ditemukan.',
         ];
     }
 
@@ -70,12 +84,12 @@ class EventReservation extends Model
 
     public function eventMenus()
     {
-        return $this->belongsToMany(EventReservation::class, 'event_reservations_menus', 'event_reservation_id', 'event_menu_id');
+        return $this->belongsToMany(EventMenu::class, 'event_reservation_menus', 'event_reservation_id', 'event_menu_id');
     }
 
     public function relations()
     {
-        return [];
+        return ['eventMenus'];
     }
 
     public function dishCategory()
